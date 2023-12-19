577d63db7235745366085dd984ad93938924e22f
alshimaamamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2023-12-19 09:58:18 -0800

    task1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,47 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Sorts linked list
+ * @list: Pointer to the pointer to the head of the list.
+ */
+
+void insertion_sort_list(listint_t **list)
+
+{
+listint_t *current, *temp;
+
+if (list == NULL || *list == NULL || (*list)->next == NULL)
+return;
+
+current = (*list)->next;
+while (current != NULL) 
+
+{
+temp = current;
+while (temp->prev != NULL && temp->n < temp->prev->n) 
+{
+if (temp->next != NULL) 
+{
+temp->next->prev = temp->prev;
+}
+temp->prev->next = temp->next;
+temp->next = temp->prev;
+temp->prev = temp->prev->prev;
+
+if (temp->prev != NULL) 
+{
+temp->prev->next = temp;
+} 
+else 
+{
+*list = temp;
+}
+if (temp->next != NULL) 
+{
+temp->next->prev = temp;
+}
+print_list(*list);
+}
+current = current->next;
+}
+}
