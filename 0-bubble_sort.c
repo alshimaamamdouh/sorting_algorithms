92b03daa45731db73da55f9f085c8c0217e84aef
alshimaamamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2023-12-19 09:20:36 -0800

    update

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -4,32 +4,45 @@
  * bubble_sort - Sorts array using Bubble Sort.
  * @array: input array .
  * @size: input size.
+ *
  */
-void bubble_sort(int *array, size_t size) {
+
+void bubble_sort(int *array, size_t size)
+{
 size_t i, j;
-int num;
-int flag;
+int num, flag;
+if (array == NULL || size < 2)
 
-if (array == NULL || size < 2) 
-return;  
+{
+return;
+}
+for (i = 0; i < size - 1; i++)
 
-for (i = 0; i < size - 1; i++) 
 {
-flag = 0;  
-for (j = 0; j < size - i - 1; j++) 
+flag = 0;
+
+for (j = 0; j < size - i - 1; j++)
+
 {
-if (array[j] > array[j + 1]) 
+if (array[j] > array[j + 1])
+
 {
 num = array[j];
 array[j] = array[j + 1];
 array[j + 1] = num;
-flag = 1; 
-print_array(array, size);  
-}
-}
-if (flag == 0) 
-break;
+flag = 1;
 
+print_array(array, size);
+
+
+}
+}
+if (flag == 0)
+
+
+{
+break;
+}
 }
 }
 
